2d0ca639dcadafee39d241d39d2a3e6febe444f4
veerasolaiyappan <veerasolaiyappan@gmail.com>
2020-10-02 13:00:38 +0530

    Create API file structure

---
+++ src/routes.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\App;
+use Slim\Interfaces\RouteCollectorProxyInterface as Group;
+
+
+
+
+
+?>
